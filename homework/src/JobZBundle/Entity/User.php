96040e7b9da82b039370b31bf55f900bfcc0c235
botondev <bbotond90@yahoo.com>
2018-06-24 13:42:50 +0200

    FOSUserBundle configured

---
+++ homework/src/JobZBundle/Entity/User.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: botondev
+ * Date: 2018.06.24.
+ * Time: 13:25
+ */
+
+namespace JobZBundle\Entity;
+
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
